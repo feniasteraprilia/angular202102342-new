b5da4139f388dcbc0693458b04483a7434571be0
nobody12-glitch <blacksentinel6@gmail.com>
2022-12-02 15:20:26 +0700

    otentikasi

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, Renderer2 } from '@angular/core';
+import { Router } from '@angular/router';
+
+declare const $ : any;
 
 @Component({
   selector: 'app-login',
@@ -7,11 +11,15 @@ import { Component, OnInit, Renderer2 } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private renderer : Renderer2) { 
-    this.renderer.addClass(document.body, "login-page");
+  constructor(private router : Router, private http : HttpClient) { 
   }
 
   ngOnInit(): void {
+  }
+
+  showPeringatanModal(message: string): void{
+    $("#peringatanModal").modal();
+    $("#pm_message").html(message);
   }
 
 }
