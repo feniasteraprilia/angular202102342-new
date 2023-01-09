9c770b1b98bd87768f7c8f962e259ba3302c38f5
nobody12-glitch <blacksentinel6@gmail.com>
2023-01-09 16:15:14 +0700

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -42,7 +42,8 @@ import { CuacaComponent } from './cuaca/cuaca.component';
     AppRoutingModule,
     HttpClientModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: !isDevMode(),
+      //enabled: !isDevMode(),
+      enabled: true,
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
